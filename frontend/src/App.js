8681a183bdad558adfe2068e1c6553a1f057ebcf
Ramo74Dixit <ramodixit76@gmail.com>
2024-08-27 21:56:44 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="font-bold text-center">
+       hiiiiiiiiiiiiiiiiiiiiiiii howwwwwwwww
+    </div>
+  );
+}
+
+export default App;
