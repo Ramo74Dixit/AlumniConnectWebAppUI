fafbbcf8093b282475673aa8d7e50ecf64f2fb0c
Ankush <ankush.spencer015@gmail.com>
2024-08-28 00:06:11 +0530

    footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="font-bold text-center">
-       hiiiiiiiiiiiiiiiiiiiiiiii howwwwwwwww
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      
+      <div className="flex-grow font-bold text-center">
+        hiiiiiiiiiiiiiiiiiiiiiiii howwwwwwwww
+      </div>
+
+      <Footer />
     </div>
   );
 }
